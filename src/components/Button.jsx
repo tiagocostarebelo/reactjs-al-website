733a399c232f2e86fc9e4f811b267388ad76ea96
Tiago Costa Rebelo <costarebelo.tiago@gmail.com>
2025-10-19 23:54:35 +0100

    Initialize Header component with navbar, links, and logo; add Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Button = ({ btnText, href, onClick }) => {
+    const baseClasses = "inline-block px-6 py-1 rounded-md font-semibold text-background bg-primary hover:bg-secondary transition";
+
+    if (href) {
+        return (
+            <a
+                href={href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={baseClasses}
+            >{btnText}</a>
+        )
+    }
+
+    return (
+        <button onClick={onClick} className={baseClasses}>
+            {btnText}
+        </button>
+    )
+}
+
+export default Button
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Button from './Button';
+import { Link } from 'react-router';
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <>
+            <h1 className="text-center font-bold text-3xl pt-6 pb-2">Anonymous Lover</h1>
+            <header className="text-center md:flex md:items-center md:justify-between px-6 py-2 md:px-8 md:py-4 space-y-4">
+                <nav className="space-x-3">
+                    <Link to="/about">About</Link>
+                    <Link to="/products">Products</Link>
+                    <Link to="/contact">Contacts</Link>
+                </nav>
+
+                <Button btnText="Go To Shop" href="https://ebay.com" />
+
+
+
+            </header>
+
+        </>
     )
 }
 
