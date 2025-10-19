f257bec8465e3d78da1d87aa73cf0fed479bf878
Tiago Costa Rebelo <costarebelo.tiago@gmail.com>
2025-10-19 23:15:56 +0100

    Initialize routes, add pages, and create initial Header and Footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-
+import HomePage from './pages/home.jsx';
+import AboutPage from './pages/about.jsx';
+import ContactPage from './pages/contact.jsx';
+import ProductsPage from './pages/products.jsx';
+import NotFoundPage from './pages/notFound.jsx';
+import { Routes, Route } from 'react-router';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
 
   return (
-    <div>My Webapp</div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes >
+      <Footer />
+    </>
   )
 }
 
